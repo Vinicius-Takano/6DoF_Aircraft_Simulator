24c68d85d7c8b16de7be2169ae78a3620a29176e
Vinicius-Takano <viniciusdtakano@hotmail.com>
2026-04-30 14:40:23 -0300

    Remove ignored files from tracking

--- main.py
+++ main.py
@@ -36,12 +36,12 @@ print()
 # Define maneuver parameters
 maneuver_params = {
     'pulse_start': 10,  # seconds
-    'pulse_duration': 1,  # seconds
+    'pulse_duration': 0.5,  # seconds
     'target_channel': 1, # 0=aileron, 1=elevator, 2=rudder, 3/4=throttle
     'pulse_magnitude': -5,  # degrees for surfaces, % for throttle
     't_start': 0.0,
-    't_end': 100,
-    'dt': 0.05
+    't_end': 60,
+    'dt': 0.01
 }
 
 # Run simulation
